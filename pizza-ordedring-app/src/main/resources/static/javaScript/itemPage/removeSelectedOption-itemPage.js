1ae22e52452432a0493dcddd90b2c547b4a377f8
Narthen <narthen34@gmail.com>
2023-04-28 15:40:38 -0400

    html js changed for itempage

--- pizza-ordedring-app/src/main/resources/static/javaScript/itemPage/removeSelectedOption-itemPage.js
+++ pizza-ordedring-app/src/main/resources/static/javaScript/itemPage/removeSelectedOption-itemPage.js
@@ -11,41 +11,49 @@
 		const removeIngredientTexts = document.querySelectorAll("#removeIngredientText");
 	
 		removeIngredientTexts.forEach((removeText) => {
+			//If it is not a dropdown.
 		  removeText.addEventListener("click", () => {
 			 if (removeText.closest("#lightNormalExtra")) {
 			    const parentDiv = removeText.closest("#lightNormalExtra");
 			
 			    const ingredientNameDiv = parentDiv.querySelector("#ingredientName");
 			    const amountDiv = parentDiv.querySelector("#amountDiv");
-			
+			    const toppPriceText = parentDiv.querySelector('#toppPriceText');
+			    
 			    if (ingredientNameDiv.style.textDecoration === "line-through") {
 			      ingredientNameDiv.style.textDecoration = "none";
 			      amountDiv.style.textDecoration = "none";
+			      toppPriceText.style.textDecoration = 'none';
 			      removeText.textContent = "Remove";
 			    } 
 			    else {
 			      ingredientNameDiv.style.textDecoration = "line-through";
 			      amountDiv.style.textDecoration = "line-through";
+			      toppPriceText.style.textDecoration = 'line-through';
 			      removeText.textContent = "Add";
 			    }
 			 }
+			 //If it is a dropdown
 			 else {
 		        const dropdownParentDiv = removeText.closest("#addonOptions-dropdownContainer");
 		      
 		        const dropdownLabelName = dropdownParentDiv.querySelector("#dropdownLabelName");
 		        const selectedOption = dropdownParentDiv.querySelector("#selectedOption");
 		        const amountDropdownDiv = dropdownParentDiv.querySelector("#amountDiv");
-	
+		        const toppPriceText = dropdownParentDiv.querySelector('#toppPriceText');
+		        
 		        if (dropdownLabelName.style.textDecoration === "line-through") {
 		          dropdownLabelName.style.textDecoration = "none";
 		          selectedOption.style.textDecoration = "none";
 		          amountDropdownDiv.style.textDecoration = "none";
+		          toppPriceText.style.textDecoration = 'none';
 		          removeText.textContent = "Remove";
 		        } 
 		        else {
 		          dropdownLabelName.style.textDecoration = "line-through";
 		          selectedOption.style.textDecoration = "line-through";
 		          amountDropdownDiv.style.textDecoration = "line-through";
+		          toppPriceText.style.textDecoration = 'line-through';
 		          removeText.textContent = "Add";
 		        }
 			 }
@@ -59,26 +67,34 @@
 			  const addOnsExtraDiv = document.querySelector('#add-ons_Extra');
 			  const addOnLightNormalExtraDivs = addOnsExtraDiv.querySelectorAll('#lightNormalExtra');
 			  
+			  //For the non-dropdown toppings
 			  addOnLightNormalExtraDivs.forEach(div => {
 				    const ingredientNameDiv = div.querySelector('#ingredientName');
 				    const amountDiv = div.querySelector('#amountDiv');
 				    const sideOfPizzaDiv = div.querySelector('#sideOfPizzaDiv');
 				    const removeIngredientText = div.querySelector('#removeIngredientText');
+				    const toppPriceText = div.querySelector('#toppPriceText');
 				    ingredientNameDiv.style.textDecoration = 'line-through';
 				    amountDiv.style.textDecoration = 'line-through';
 				    sideOfPizzaDiv.style.textDecoration = 'line-through';
+				    toppPriceText.style.textDecoration = 'line-through';
 				    removeIngredientText.textContent = 'Add';
+				    
+				    
 			  });
 			  
+			//For the dropdown toppings
 			  const addonDropdownDivs = addOnsExtraDiv.querySelectorAll('#addonOptions-dropdownContainer');
 			  addonDropdownDivs.forEach(div => {
 				    const ingredientNameDiv = div.querySelector('#dropdownLabelName');
 				    const amountDiv = div.querySelector('#amountDiv');
 				    const selectedOptionDiv = div.querySelector('#selectedOption');
 				    const removeIngredientText = div.querySelector('#removeIngredientText');
+				    const toppPriceText = div.querySelector('#toppPriceText');
 				    ingredientNameDiv.style.textDecoration = 'line-through';
 				    amountDiv.style.textDecoration = 'line-through';
 				    selectedOptionDiv.style.textDecoration = 'line-through';
+				    toppPriceText.style.textDecoration = 'line-through';
 				    removeIngredientText.textContent = 'Add';
 			  });
 			  
